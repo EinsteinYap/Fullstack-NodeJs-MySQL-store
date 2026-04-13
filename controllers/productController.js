37072efbbd9e94504826fa9ec2691d99c7103de6
EinsteinYap <einsteinyap2014@gmail.com>
2026-04-14 01:13:17 +0800

    removed dummy data

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,36 +1,5 @@
 const Products = require('../models/products');
-// const products = [
-//     {
-//         id: 1,
-//         productname:"Apple",
-//         price: 100,
-//         img:"apples.jpg"
-//     },
-//     {
-//         id: 2,
-//         productname:"Bananas",
-//         price: 50,
-//         img:"bananas.jpg"
-//     },
-//     {
-//         id: 3,
-//         productname:"Orange",
-//         price: 150,
-//         img:"orange.jpg"
-//     },
-//     {
-//         id: 4,
-//         productname:"Pineapple",
-//         price: 200,
-//         img:"pineapple.jpg"
-//     },
-//     {
-//         id: 5,  
-//         productname:"Strawberry",
-//         price: 250,
-//         img:"strawberry.jpg"
-//     }
-// ];
+
 const products = [];
     
 
